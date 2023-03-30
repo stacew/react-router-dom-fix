868948b275d0f7f69a60572b6084e4d8feb76cb2
stacew <chunrans4@naver.com>
2023-03-31 00:42:52 +0900

    first commit

--- README.md
+++ README.md
@@ -1,29 +1,11 @@
 # react-router-dom-fix
 
-> Made with create-react-library
+My personal react-router-dom adapter, only a few available.
+If the address changes, useNavigate() will not invoke the re-render.
+
+### `Warning: I haven't had any problems yet, but you have to be careful.`
 
 [![NPM](https://img.shields.io/npm/v/react-router-dom-fix.svg)](https://www.npmjs.com/package/react-router-dom-fix) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
-
-## Install
-
-```bash
-npm install --save react-router-dom-fix
-```
-
-## Usage
-
-```tsx
-import React, { Component } from 'react'
-
-import MyComponent from 'react-router-dom-fix'
-import 'react-router-dom-fix/dist/index.css'
-
-class Example extends Component {
-  render() {
-    return <MyComponent />
-  }
-}
-```
 
 ## License
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,42 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { createContext, ReactNode, useCallback, useContext } from 'react'
+import {
+  BrowserRouter,
+  NavigateFunction,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate
+} from 'react-router-dom'
 
-interface Props {
-  text: string
+const navigateFix = createContext<NavigateFunction>(() => {})
+
+/**
+ * I haven't had any problems yet, but you have to be careful.
+ */
+const useNavigateFix = (): NavigateFunction => useContext(navigateFix)
+
+const BrowserRouterFix = (props: { children: ReactNode }) => {
+  return (
+    <BrowserRouter>
+      <ProviderNavigateFix>{props.children}</ProviderNavigateFix>
+    </BrowserRouter>
+  )
+  function ProviderNavigateFix(props: { children: ReactNode }) {
+    const navigate = useNavigate()
+    return (
+      <navigateFix.Provider value={useCallback(navigate, [])}>
+        {props.children}
+      </navigateFix.Provider>
+    )
+  }
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export {
+  useNavigate,
+  useNavigateFix,
+  BrowserRouterFix,
+  Route,
+  Routes,
+  useLocation
 }
